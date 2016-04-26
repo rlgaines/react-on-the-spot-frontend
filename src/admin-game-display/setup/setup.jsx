d96e08992f2636557c38b8cb850a0f65c6156cdd
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-26 13:46:54 -0600

    get stuff working

---
+++ src/admin-game-display/setup/setup.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+
+class AdminGame extends Component {
+  render() {
+    if (!this.props.SelectedQuestion) {
+        return (
+            <div>
+                <h3>Hey</h3>
+              </div>
+        )
+    }
+
+    return (
+      <div>
+        <div className="row">
+            <h3><i className="fa fa-cogs" aria-hidden="true"></i></h3>
+        </div>
+        <div className="row">
+            <aside className="four columns">
+                <h3>Score Board</h3>
+                <ul>
+                    <li>(team)  (score)</li>
+                    <li>(team)  (score)</li>
+                    <li>(team)  (score)</li>
+                    <li>(team)  (score)</li>
+                </ul>
+            </aside>
+            <main className="eight columns">
+                <div className="row" id="questionZone">
+                    <h3>Question</h3>
+                </div>
+                    {this.props.children}
+            </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default AdminGame;
