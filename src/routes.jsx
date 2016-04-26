37f129d52191b97fc9e3de7f5fbd974eafdd2a85
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-26 13:25:57 -0600

    additional routes. router shouldn't break now

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,7 +9,10 @@ import Home from './home/home';
 import TeamSignIn from './team-signin/team-signin';
 import TeamDisplay from './team-display/team-display';
 import AdminGame from './admin-game-display/admin-game-display';
-import GameBoard from './'
+import GameBoard from './admin-game-display/board/board';
+import GameSetup from './admin-game-display/setup/setup';
+import GameAnswers from './admin-game-display/answers/answers';
+
 
 export default  (
     <Route path='/' component={App}>
