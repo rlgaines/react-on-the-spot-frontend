93b472cde6b8f32d354fe52cd1bfc743c778fd6e
rlgaines <robertlg35@gmail.com>
2016-04-26 10:53:20 -0600

    home log in page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './App/app';
 import CSV from './csv/csv-container';
-
+import Home from './home/home';
 
 export default  (
     <Route path='/' component={App}>
+    	<IndexRoute component={Home} />
         <Route path="csv-upload" component={CSV} />
     </Route>
 );
