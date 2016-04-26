0bbf0c18458cdc9c07be9113289819561e5c2b36
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-26 10:57:00 -0600

    routes updated

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,17 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './App/app';
 import CSV from './csv/csv-container';
+<<<<<<< Updated upstream
 import Home from './home/home';
+=======
+import TeamSignIn from './team-signin/team-signin';
+>>>>>>> Stashed changes
 
 export default  (
     <Route path='/' component={App}>
     	<IndexRoute component={Home} />
         <Route path="csv-upload" component={CSV} />
+        <Route path="team" component={TeamSignIn} />
     </Route>
 );
 
